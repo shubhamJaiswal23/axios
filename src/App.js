98a19ce09079ca9fec11ab7dff739b9a4326da51
Shubham Jaiswal <jaiswalshubham31012000@gmail.com>
2024-01-28 12:52:38 +0530

    get request using axios

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import Title from './components/Title';
+import SetUp from './examples/1-first-request';
+
 function App() {
   return (
     <main>
       <Title />
+      <SetUp />
     </main>
   );
 }
